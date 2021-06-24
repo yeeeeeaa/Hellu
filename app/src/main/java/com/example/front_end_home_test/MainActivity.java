7eb4a4286f693463eb832f327f26a77483cbceea
hoeun <dotothy0605@gmail.com>
2021-06-24 16:40:15 +0900

    bottom_navigation move

--- app/src/main/java/com/example/front_end_home_test/MainActivity.java
+++ app/src/main/java/com/example/front_end_home_test/MainActivity.java
@@ -1,98 +1,77 @@
 package com.example.front_end_home_test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    //하단 네비게이션
+    private BottomNavigationView mBottomNV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //홈에 있는 매일 인증(daily_stamp)버튼을 누르면 매일 인증 화면으로 넘어감
-        Button imageButton = findViewById(R.id.daily_stamp_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        //하단 네비게이션 화면 선택하면 눌리는 함수
+        mBottomNV = findViewById(R.id.bottom_navigation);
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() { //NavigationItemSelecte
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), daily_stamp.class);
-                startActivity(intent);
-            }
-        });
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                BottomNavigate(menuItem.getItemId());
 
-        //홈에 있는 이벤트 인증(event_stamp)버튼을 누르면 이벤트 인증 화면으로 넘어감
-        imageButton = findViewById(R.id.event_stamp_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), event_stamp.class);
-                startActivity(intent);
-            }
-        });
 
-        //홈에 있는 플랜 선택(plan_choose)버튼을 누르면 플랜 선택 화면으로 넘어감
-        imageButton = findViewById(R.id.plan_choose_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), plan_choose.class);
-                startActivity(intent);
+                return true;
             }
         });
+        mBottomNV.setSelectedItemId(R.id.home);
 
-        //홈에 있는 소통(community)버튼을 누르면 커뮤니티 화면으로 넘어감
-        imageButton = findViewById(R.id.community_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), community.class);
-                startActivity(intent);
-            }
-        });
-        
-        //홈에 있는 인바디 체크(inbody_check)버튼을 누르면 인바디 체크 화면으로 넘어감
-        imageButton = findViewById(R.id.inbody_check_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), inbody_check.class);
-                startActivity(intent);
-            }
-        });
+    }
+    //BottomNavigation 페이지 변경
+    private void BottomNavigate(int id) {
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        //홈에 있는 랭킹(ranking)버튼을 누르면 랭킹 화면으로 넘어감
-        imageButton = findViewById(R.id.ranking_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ranking.class);
-                startActivity(intent);
-            }
-        });
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
 
-        //홈에 있는 걷기 운동(walking)버튼을 누르면 걷기 운동 화면으로 넘어감
-        imageButton = findViewById(R.id.walking_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), walking.class);
-                startActivity(intent);
-            }
-        });
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.home) {
+                fragment = new fragment_home();
 
-        //홈에 있는 역대 기록(record)버튼을 누르면 역대 기록 화면으로 넘어감
-        imageButton = findViewById(R.id.record_button);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), record.class);
-                startActivity(intent);
+            } else if (id == R.id.feed){
+
+                fragment = new fragment_feed();
+            }else if (id == R.id.mypage){
+                fragment = new fragment_mypage();
+            }else{
+                fragment=new fragment_setting();
             }
-        });
+
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+
+
     }
 
 }
