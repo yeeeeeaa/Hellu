840e04bd80a17a23a06b5f97441ec688f0e94a90
hoeun <dotothy0605@gmail.com>
2021-06-24 20:10:16 +0900

    8button and navigation move

--- app/src/main/java/com/example/front_end_home_test/fragment_home.java
+++ app/src/main/java/com/example/front_end_home_test/fragment_home.java
@@ -18,89 +18,91 @@ public class fragment_home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        /* 미안해요 은서의 버튼 능력을 상실했어요....정말 미안해요....
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //미안해요 은서의 버튼 능력을 상실했어요....정말 미안해요....
         //홈에 있는 매일 인증(daily_stamp)버튼을 누르면 매일 인증 화면으로 넘어감
-        Button imageButton = findViewById(R.id.daily_stamp_button);
+        Button imageButton = view.findViewById(R.id.daily_stamp_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), daily_stamp.class);
+                Intent intent = new Intent(getActivity(), daily_stamp.class);
                 startActivity(intent);
             }
         });
 
         //홈에 있는 이벤트 인증(event_stamp)버튼을 누르면 이벤트 인증 화면으로 넘어감
-        imageButton = findViewById(R.id.event_stamp_button);
+        imageButton = view.findViewById(R.id.event_stamp_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), event_stamp.class);
+                Intent intent = new Intent(getActivity(), event_stamp.class);
                 startActivity(intent);
             }
         });
 
         //홈에 있는 플랜 선택(plan_choose)버튼을 누르면 플랜 선택 화면으로 넘어감
-        imageButton = findViewById(R.id.plan_choose_button);
+        imageButton = view.findViewById(R.id.plan_choose_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), plan_choose.class);
+                Intent intent = new Intent(getActivity(), plan_choose.class);
                 startActivity(intent);
             }
         });
 
         //홈에 있는 소통(community)버튼을 누르면 커뮤니티 화면으로 넘어감
-        imageButton = findViewById(R.id.community_button);
+        imageButton = view.findViewById(R.id.community_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), community.class);
+                Intent intent = new Intent(getActivity(), community.class);
                 startActivity(intent);
             }
         });
 
         //홈에 있는 인바디 체크(inbody_check)버튼을 누르면 인바디 체크 화면으로 넘어감
-        imageButton = findViewById(R.id.inbody_check_button);
+        imageButton = view.findViewById(R.id.inbody_check_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), inbody_check.class);
+                Intent intent = new Intent(getActivity(), inbody_check.class);
                 startActivity(intent);
             }
         });
 
         //홈에 있는 랭킹(ranking)버튼을 누르면 랭킹 화면으로 넘어감
-        imageButton = findViewById(R.id.ranking_button);
+        imageButton = view.findViewById(R.id.ranking_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), ranking.class);
+                Intent intent = new Intent(getActivity(), ranking.class);
                 startActivity(intent);
             }
         });
 
         //홈에 있는 걷기 운동(walking)버튼을 누르면 걷기 운동 화면으로 넘어감
-        imageButton = findViewById(R.id.walking_button);
+        imageButton = view.findViewById(R.id.walking_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), walking.class);
+                Intent intent = new Intent(getActivity(), walking.class);
                 startActivity(intent);
             }
         });
 
         //홈에 있는 역대 기록(record)버튼을 누르면 역대 기록 화면으로 넘어감
-        imageButton = findViewById(R.id.record_button);
+        imageButton = view.findViewById(R.id.record_button);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), record.class);
+                Intent intent = new Intent(getActivity(), record.class);
                 startActivity(intent);
             }
         });
-         */
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
