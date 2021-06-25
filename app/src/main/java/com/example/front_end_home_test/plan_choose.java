d07fdffed469c2603c408fa2e1cae17ba419bf90
es <eun20181103@gmail.com>
2021-06-25 12:41:43 +0900

    plan_choose two button clear

--- app/src/main/java/com/example/front_end_home_test/plan_choose.java
+++ app/src/main/java/com/example/front_end_home_test/plan_choose.java
@@ -24,6 +24,16 @@ public class plan_choose extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //플랜추천에 있는 'ai의 정확한 인바디 분석'버튼을 누르면 pchoose_inbody창으로 넘어감
+        imageButton = findViewById(R.id.pch_button_inbody);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), pchoose_inbody_plan.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
