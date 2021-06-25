efa758ac2bbccee0eb8979ee1c774dcb76b6a590
es <eun20181103@gmail.com>
2021-06-25 12:29:19 +0900

    pchoose_QnA_plan only button

---
+++ app/src/main/java/com/example/front_end_home_test/pchoose_QnA_plan.java
@@ -0,0 +1,16 @@
+package com.example.front_end_home_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pchoose_QnA_plan extends AppCompatActivity {
+
+    //플랜추천 화면에서 'ai로 선별해주는 플랜' 버튼을 누르면 나오는 QnA 화면
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pchoose__qn_a_plan);
+    }
+}
--- app/src/main/java/com/example/front_end_home_test/plan_choose.java
+++ app/src/main/java/com/example/front_end_home_test/plan_choose.java
@@ -2,14 +2,28 @@ package com.example.front_end_home_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class plan_choose extends AppCompatActivity {
 
-    //플랜선택.java
+    //플랜추천.java
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_choose);
     }
+
+   /* //플랜추천에 있는 'ai로 선별해주는 플랜'버튼을 누르면 pchoose_QnA_plan창으로 넘어감
+    Button imageButton = findViewById(R.id.pch_button_plan);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), pchoose_QnA_plan.class);
+            startActivity(intent);
+        }
+    });
+    */
 }
