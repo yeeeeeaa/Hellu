fe4616be153c4b0af58abd73550a523b41e2b206
hoeun <dotothy0605@gmail.com>
2021-07-20 20:23:46 +0900

    daily_write_make2

--- app/src/main/java/com/example/front_end_home_test/daily_stamp.java
+++ app/src/main/java/com/example/front_end_home_test/daily_stamp.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ActivityGroup;
 import android.content.Intent;
@@ -14,6 +16,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class daily_stamp extends AppCompatActivity {
 
@@ -25,6 +30,14 @@ public class daily_stamp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_stamp);
+
+        RecyclerView recyclerView = findViewById(R.id.daily_recyclerview); //리사이클러뷰 연결
+        /*recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); //레이아웃 매니저와 리사이클러뷰 연결
+
+        daily_stamp_write_noteAdapter adapter = new daily_stamp_write_noteAdapter(this, createItemList(),R.layout.daily_stamp_write_item);
+        recyclerView.setAdapter(adapter); //어댑터와 연결*/
+
 
 
         //하단 네비게이션 화면을 선택하면 실행됨
@@ -54,4 +67,12 @@ public class daily_stamp extends AppCompatActivity {
             startActivity(intent);
         }
     }
+    /*private List<String> createItemList() {
+        List<String> items = new ArrayList<>();
+        for(int i=0; i<20; i++){
+            items.add("아이템"+i); //아이템 19개만큼 추가
+        }
+        return items;
+    } //위에서 쓴 createItemList()라는 메서드를 정의*/
+
 }
--- app/src/main/java/com/example/front_end_home_test/daily_stamp_write_noteAdapter.java
+++ app/src/main/java/com/example/front_end_home_test/daily_stamp_write_noteAdapter.java
@@ -1,40 +1,66 @@
 package com.example.front_end_home_test;
 
+import android.content.Context;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
 public class daily_stamp_write_noteAdapter extends RecyclerView.Adapter<daily_stamp_write_noteAdapter.ViewHolder> {
     List<daily_stamp_write_note> items;
+    Context context;
     int itemLayout;
 
-    public daily_stamp_write_noteAdapter(List<daily_stamp_write_note> items, int itemLayout) {
+    public daily_stamp_write_noteAdapter(Context context, List<daily_stamp_write_note> items, int itemLayout) {
+        this.context = context;
         this.items = items;
         this.itemLayout = itemLayout;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.daily_stamp_write_item, viewGroup, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        daily_stamp_write_note item = items.get(position);
+        //holder.setItem(item);
+        holder.daily_image.setImageResource(item.getPicture());
+        holder.daily_date.setText(item.getCreateDateStr());
+        holder.daily_text.setText(item.getContents());
+        holder.itemView.setTag(item);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView daily_image;
+        TextView daily_date;
+        TextView daily_text;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            daily_image = itemView.findViewById(R.id.daily_write_image);
+            daily_date = itemView.findViewById(R.id.daily_write_date);
+            daily_text = itemView.findViewById(R.id.daily_write_text);
+
         }
+
     }
+
 }
