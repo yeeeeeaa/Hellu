d577c37e8174072df7407c0adebed354ba9ad8a4
hoeuni <euni.gmail.com>
2021-07-29 22:48:08 +0900

    daily_write_comment

--- app/src/main/java/com/example/HelluApp/DailyStamp/daily_stamp_write.java
+++ app/src/main/java/com/example/HelluApp/DailyStamp/daily_stamp_write.java
@@ -6,15 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.HelluApp.R;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
 public class daily_stamp_write extends AppCompatActivity {
 
     //매일 인증 글쓰기.java
+
+    String ID;                  //글 번호-이건 파이어베이스에 필요한지는 모르겠어요
+    String Title;               //글 제목
+    String Content;             //글 내용
+    String Picture;             //글 사진 경로
+    String DateWrite;           //글에 대한 날짜
 
     Button save_button;         //매일인증 저장하기 버튼
     Button gallery;             //갤러리 열기 버튼
@@ -29,6 +41,7 @@ public class daily_stamp_write extends AppCompatActivity {
         FirebaseApp.initializeApp(this);        //이것도 plan_choose.java에 있어서 가져왔습니다.
         setContentView(R.layout.activity_daily_stamp_write);
 
+        //버튼을 누르면 갤러리로 넘어가는 코드 by 예린
         gallery = findViewById(R.id.daily_write_image_button);
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,12 +53,33 @@ public class daily_stamp_write extends AppCompatActivity {
             }
         });
 
+
         save_button = findViewById(R.id.daily_write_save_button);
         save_button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
 
+                //모르지만 예린이 코드 훔쳐오기
+                //Input_daily();
+                /*HashMap<String, Object> Daily_write = new HashMap<>();
+                Daily_write.put("글 번호", ID);
+                Daily_write.put("제목", Title);
+                Daily_write.put("내용", Content);
+                Daily_write.put("사진", Picture);
+                Daily_write.put("날짜", DateWrite);
+
+                databaseReference.child("User_Write").push().setValue(Daily_write);*/
             }
         });
+    }
+    public void Input_daily(){
+
+        /*
+        //제목 입력
+        EditText Title = findViewById(R.id.daily_write_title);
+
+        //내용 입력
+        EditText Context = findViewById(R.id.daily_write_context);
+        */
     }
 
 }
