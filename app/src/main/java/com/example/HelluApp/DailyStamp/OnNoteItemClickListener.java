b818679c6c7dac76ea79dbcd45733f19d0d5d52f
hoeuni <euni.gmail.com>
2021-08-26 20:53:58 +0900

    daily_stamp_write_item.xml_fixed

--- app/src/main/java/com/example/HelluApp/DailyStamp/OnNoteItemClickListener.java
+++ app/src/main/java/com/example/HelluApp/DailyStamp/OnNoteItemClickListener.java
@@ -2,7 +2,8 @@ package com.example.HelluApp.DailyStamp;
 
 import android.view.View;
 
+/*
 public interface OnNoteItemClickListener {
     public void onItemClick(daily_stamp_write_noteAdapter.ViewHolder holder, View view, int position);
 }
-
+ */
