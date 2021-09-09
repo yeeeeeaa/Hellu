da3b3c7544807b1980f04d0498320c08f577095b
hoeuni <euni.gmail.com>
2021-09-09 23:53:36 +0900

    fragment_feed ActionBar

---
+++ app/src/main/java/com/example/HelluApp/MainFragment/feed/feed_recent.java
@@ -0,0 +1,21 @@
+package com.example.HelluApp.MainFragment.feed;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.HelluApp.R;
+
+
+public class feed_recent extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed_recent, container, false);
+    }
+}
