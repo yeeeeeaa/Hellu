02d8cf0636a046393a0163403cc35a15c7d4015d
seoyeon <choesy@suwon.ac.kr>
2021-09-19 15:26:53 +0900

    modify plan save type

--- app/src/main/java/com/example/HelluApp/plan_choose.java
+++ app/src/main/java/com/example/HelluApp/plan_choose.java
@@ -18,14 +18,24 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions;
+import com.google.firebase.ml.modeldownloader.DownloadType;
+import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader;
 
+import org.tensorflow.lite.Interpreter;
+
+import java.io.File;
 import java.text.BreakIterator;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class plan_choose extends AppCompatActivity {
+    float max_result;
+    int index;
+    
     String Gender = "";                 //성별
     String Age = "";                    //나이
     String EditHeight = "";             //신장, 키 (현재 내 키를 쓴다는 의미)
@@ -38,11 +48,12 @@ public class plan_choose extends AppCompatActivity {
     String amountOfExercise = "";       // 하루 목표 운동량
     ArrayList<String> mealTime = new ArrayList<>();          //식사 시간
     String purposeOfExercise = "";      // 운동 목적(Motivation)
+    String exercise;                    // 추천 운동
     Button result_btn;
     ArrayList<String> meal_feedback = new ArrayList<>();   // 식단가이드(피드백)
 
-    //    private final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-//    private final DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private final DatabaseReference databaseReference = firebaseDatabase.getReference();
     // 파이어베이스 유저 아이디 가져오는 코드인 듯? From. daily_stamp_write.java
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser user = mAuth.getCurrentUser();
@@ -57,51 +68,40 @@ public class plan_choose extends AppCompatActivity {
         result_btn = findViewById(R.id.pch_result_button);
 
         result_btn.setOnClickListener(v -> {
-
             Checked();
 
             // 현재 체중이랑 그냥 체중은 값은 다르고 용도가 다름
             HashMap<String, Object> Plan_result = new HashMap<>();
-//            Plan_result.put("성별", Gender);
-//            Plan_result.put("나이", Age);
-//            Plan_result.put("신장", EditHeight);
-//            Plan_result.put("체중", EditWeight);
-//            Plan_result.put("목표 감량 체중", LoseWeight);
-//            Plan_result.put("현재 체중", PresentWeight);
-//            Plan_result.put("목표 체중", GoalWeight);
-//            Plan_result.put("운동 주 횟수", numberOfWeekOfExercise);
-//            Plan_result.put("하루 목표 운동량", amountOfExercise);
-//            Plan_result.put("평소 활동량", normalActivity);
-//            Plan_result.put("식사 시간", mealTime);
-//            Plan_result.put("운동 목적", purposeOfExercise);
-//            Plan_result.put("식단 가이드", meal_feedback);
+            Plan_result.put("성별", Gender);
+            Plan_result.put("나이", Age);
+            Plan_result.put("신장", EditHeight);
+            Plan_result.put("체중", EditWeight);
+            Plan_result.put("목표 감량 체중", LoseWeight);
+            Plan_result.put("현재 체중", PresentWeight);
+            Plan_result.put("목표 체중", GoalWeight);
+            Plan_result.put("운동 주 횟수", numberOfWeekOfExercise);
+            Plan_result.put("하루 목표 운동량", amountOfExercise);
+            Plan_result.put("평소 활동량", normalActivity);
+            Plan_result.put("식사 시간", mealTime);
+            Plan_result.put("운동 목적", purposeOfExercise);
+            Plan_result.put("식단 가이드", meal_feedback);
+
+            // 선호 운동 출력
+            prefer_Exercises(Gender, Age, normalActivity, purposeOfExercise);
+            // 기초대사량 계산
+            String BEE = CalculateBEE(Gender, EditWeight, EditHeight, Age);
+            Plan_result.put("기초대사량", BEE);
 
 //            databaseReference.child("User_Plan").push().setValue(Plan_result);
+            databaseReference.child("User_Plan").child(Uid).setValue(Plan_result);
 
+            // 다음 화면으로 넘어감.
             Intent intent = new Intent(getApplicationContext(), plan_choose_result.class);
-
-            // plan_choose_result로 값을 넘김.
-            intent.putExtra("EditWeight", EditWeight);
-            intent.putExtra("GoalWeight", GoalWeight);
-            intent.putExtra("numberOfWeekOfExercise", numberOfWeekOfExercise);
-            intent.putExtra("normalActivity", normalActivity);
-            intent.putExtra("Gender", Gender);
-            intent.putExtra("EditHeight", EditHeight);
-            intent.putExtra("Age", Age);
-            intent.putExtra("purposeOfExercise", purposeOfExercise);
-            intent.putExtra("meal_feedback", meal_feedback);
-            intent.putExtra("LoseWeight", LoseWeight);
-            intent.putExtra("PresentWeight", PresentWeight);
-            intent.putExtra("amountOfExercise", amountOfExercise);
-            intent.putExtra("mealTime", mealTime);
-            intent.putExtra("meal_feedback", meal_feedback);
-
             startActivity(intent);
         });
     }
 
     public void Checked() {
-
         //성별 선택 (단일 선택)
         RadioButton optionG1 = findViewById(R.id.man);                  //optionG1 옵션젠더1=맨
         RadioButton optionG2 = findViewById(R.id.woman);                //optionG2 옵션젠더2=우먼
@@ -315,4 +315,177 @@ public class plan_choose extends AppCompatActivity {
         }
     }
 
+    // AI에 필요한 입력 값: 성별, 나이(구간), 평소 활동량, 운동 목적
+    public void prefer_Exercises(String Gender, String Age, String Time_spent, String Motivation){
+        CustomModelDownloadConditions conditions = new CustomModelDownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        FirebaseModelDownloader.getInstance()
+                .getModel("model_surveyAI", DownloadType.LOCAL_MODEL, conditions)
+                .addOnSuccessListener(model -> {
+                    // Download complete. Depending on your app, you could enable the ML
+                    // feature, or switch from the local model to the remote model, etc.
+
+                    // The CustomModel object contains the local path of the model file,
+                    // which you can use to instantiate a TensorFlow Lite interpreter.
+                    File modelFile = model.getFile();
+                    if (modelFile != null) {
+                        Interpreter interpreter = new Interpreter(modelFile);
+
+                        // 1. 라디오 버튼의 값을 하나하나 받아서 input에 넣을 값 구분.
+                        // 2. 위치에 맞게 배열
+                        // 3. 그리고 input에 넣기.
+                        // 모델의 Input
+                        //float[][] input = {{1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+                        // 입력값 모두 0으로 초기화
+                        float[][] input = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+                        float[][] output = new float[1][7]; // 모델의 Output
+
+                        input = surveyValueRetn(Gender, Age, Time_spent, Motivation, input);
+
+                        // 결과 출력
+                        //print(interpreter,input,output);
+
+                        interpreter.run(input, output); // 모델 실행
+
+                        // 모델 결과값 출력하기
+                        for (int i = 0; i < output[0].length; i++) {
+                            // 결과값 중에 최댓값 선정하기
+                            if(i == 0){
+                                index = 0;
+                                max_result = output[0][i];
+                            }
+                            if(max_result < output[0][i]){
+                                index = i;
+                                max_result = output[0][i];
+                            }
+                        }
+
+                        switch(index){
+                            case 0:
+                                exercise = "체육관 운동";
+                                break;
+
+                            case 1:
+                                exercise = "웨이트 트레이닝";
+                                break;
+
+                            case 2:
+                                exercise = "수영";
+                                break;
+
+                            case 3:
+                                exercise = "팀 스포츠";
+                                break;
+
+                            case 4:
+                                exercise = "조깅";
+                                break;
+
+                            case 5:
+                                exercise = "요가";
+                                break;
+
+                            case 6:
+                                exercise = "줌바 댄스";
+                                break;
+
+                            default:
+                                break;
+                        }
+                        /*
+                        for(int i = 0; i < input[0].length; i++){    // 입력값 테스트
+                            System.out.println(input[0][i] + "\n");
+                        }
+                        */
+                        // 변수에 저장해서 출력하기
+//                        prefer_ex.setText(exercise);
+
+                        // 파베 저장 코드
+                        databaseReference.child("User_Plan").child(Uid).child("추천운동").setValue(exercise);
+
+                        interpreter.close();    // 인터프리터 종료
+                    }
+                });
+    }
+
+    public String CalculateBEE(String Gender, String strWeight, String strHeight, String strAge){
+        double resultBEE = 0;
+        // 소수점 두 번째 자리까지
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        if(Gender.equals("여")){
+            resultBEE = 655.1 + (9.56 * Double.parseDouble(strWeight)) + (1.85 * Double.parseDouble(strHeight)) - (4.68 * Integer.parseInt(strAge));
+        }else if(Gender.equals("남")){
+            resultBEE = 66.5 + (13.75 * Double.parseDouble(strWeight)) + (5 * Double.parseDouble(strHeight)) - (6.76 * Integer.parseInt(strAge));
+        }
+
+        String BEE = decimalFormat.format(resultBEE);
+
+        return BEE;
+    }
+
+    float[][] surveyValueRetn(String Gender, String Age, String Time_spent,
+                              String Motivation, float[][] input){
+        // Gender
+        if(Gender.equals("여")){
+            input[0][0] = 1;
+        }else if (Gender.equals("남")){
+            input[0][1] = 1;
+        }
+
+        // Age
+        int intAge = Integer.parseInt(Age);
+
+        if(intAge >= 15 && intAge <= 18){
+            input[0][2] = 1;
+        }else if(intAge >= 19 && intAge <= 25){
+            input[0][3] = 1;
+        }else if(intAge >= 26 && intAge < 30){
+            input[0][4] = 1;
+        }else if(intAge >= 30 && intAge < 40){
+            input[0][5] = 1;
+        }else if(intAge >= 40){
+            input[0][6] = 1;
+        }
+
+        // Time_spent
+        switch (Time_spent) {
+            case "1시간":
+                input[0][7] = 1;
+                break;
+            case "2시간":
+                input[0][8] = 1;
+                break;
+            case "3시간 이상":
+                input[0][9] = 1;
+                break;
+            case "30분":
+                input[0][10] = 1;
+                break;
+            case "운동을 하지 않는다.":
+                input[0][11] = 1;
+                break;
+        }
+
+        // Motivation
+        switch (Motivation) {
+            case "건강":
+                input[0][12] = 1;
+                break;
+            case "균형":
+                input[0][13] = 1;
+                break;
+            case "체력 단련":
+                input[0][14] = 1;
+                break;
+            case "다이어트":
+                input[0][15] = 1;
+                break;
+            case "스트레스 해소":
+                input[0][16] = 1;
+                break;
+        }
+        return input;
+    }
 }
