6392b85af2d456b3418a99798f56e8444527482e
hoeuni <euni.gmail.com>
2021-09-20 01:33:58 +0900

    metaverse_trainingroom add

--- app/src/main/java/com/example/HelluApp/Metaverse/metaverse.java
+++ app/src/main/java/com/example/HelluApp/Metaverse/metaverse.java
@@ -4,20 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.HelluApp.DailyStamp.daily_stamp;
 import com.example.HelluApp.R;
 
 public class metaverse extends AppCompatActivity {
     RecyclerView recyclerView;
     metaverse_noteAdapter adapter;
+    public static final int REQUEST_CODE_MENU = 101;
 
     //메타버스.java
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metaverse);
+
+
 
        recyclerView = findViewById(R.id.meta_recylerview);
 
@@ -35,6 +42,18 @@ public class metaverse extends AppCompatActivity {
 
 
         recyclerView.setAdapter(adapter);
+
+        //메타버스 "방 입장하기"을 누르면 운동 설명+링크 입력창+입력버튼 나타나는 화면 띄워주기
+//        Button button = findViewById(R.id.trainingClassEnterButton);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), metaverse_trainingroom.class);
+//                //startActivity(intent);
+//                startActivityForResult(intent, REQUEST_CODE_MENU);
+//            }
+//        });
+
     }
 
 }
--- app/src/main/java/com/example/HelluApp/Metaverse/metaverse_noteAdapter.java
+++ app/src/main/java/com/example/HelluApp/Metaverse/metaverse_noteAdapter.java
@@ -1,8 +1,10 @@
 package com.example.HelluApp.Metaverse;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +25,15 @@ public class metaverse_noteAdapter extends RecyclerView.Adapter<metaverse_noteAd
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
         View itemView = inflater.inflate(R.layout.metaverse_item, viewGroup, false);
 
+        Button button = itemView.findViewById(R.id.trainingClassEnterButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(inflater.getContext(), metaverse_trainingroom.class);
+                inflater.getContext().startActivity(intent);
+            }
+        });
+
         return new ViewHolder(itemView);
     }
 
@@ -30,6 +41,8 @@ public class metaverse_noteAdapter extends RecyclerView.Adapter<metaverse_noteAd
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         metaverse_note item = items.get(position);
         holder.setItem(item);
+
+
     }
 
     @Override
@@ -58,11 +71,14 @@ public class metaverse_noteAdapter extends RecyclerView.Adapter<metaverse_noteAd
 
             className = itemView.findViewById(R.id.trainingClassName);
             trainerName = itemView.findViewById(R.id.trainerTName);
+
         }
 
         public void setItem(metaverse_note item) {
             className.setText(item.getTrainingClass());
             trainerName.setText(item.getTrainerName());
         }
+
+
     }
 }
