26ae7da089b07c0edfc8bb28ec56ba4e11aba1dd
이다영 <babyhiken@gmail.com>
2021-09-29 12:28:31 +0900

    realrealendendend

--- app/src/main/java/com/example/HelluApp/Walking/walking_ranking_checking.java
+++ app/src/main/java/com/example/HelluApp/Walking/walking_ranking_checking.java
@@ -11,6 +11,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -75,6 +76,24 @@ public class walking_ranking_checking extends AppCompatActivity implements Senso
         if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
             tvStepCount.setText("Step Count : " + ((int) event.values[0]));
             walking = String.valueOf(Integer.valueOf((int) event.values[0]));
+            FirebaseDatabase.getInstance().getReference("User").addValueEventListener(new ValueEventListener() {
+                @Override
+                //리스너는 이벤트 발생 시점에 데이터베이스에서 지정된 위치에 있던 데이터를 포함하는 DataSnapshot을 수신한다.
+                //스냅샷에 대해 getValue()를 호출하면 데이터의 자바 객체 표현이 반환된다.
+                //해당 위치에 데이터가 없는 경우 getValue()를 호출하면 null이 반환된다.
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("User").child(Uid);
+                    Map<String, Object> walkingUpdates = new HashMap<>();
+                    walkingUpdates.put("Walking", walking);
+                    usersRef.updateChildren(walkingUpdates);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Getting Post failed, log a message
+                    Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+                }
+            });
         }
     }
 
@@ -83,25 +102,7 @@ public class walking_ranking_checking extends AppCompatActivity implements Senso
 
     }
     public void clickSend(View view) {
-        FirebaseDatabase.getInstance().getReference("User").addValueEventListener(new ValueEventListener() {
-            @Override
-            //리스너는 이벤트 발생 시점에 데이터베이스에서 지정된 위치에 있던 데이터를 포함하는 DataSnapshot을 수신한다.
-            //스냅샷에 대해 getValue()를 호출하면 데이터의 자바 객체 표현이 반환된다.
-            //해당 위치에 데이터가 없는 경우 getValue()를 호출하면 null이 반환된다.
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("User").child(Uid);
-                Map<String, Object> walkingUpdates = new HashMap<>();
-                walkingUpdates.put("Walking", walking);
-                usersRef.updateChildren(walkingUpdates);
-                onBackPressed();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
-            }
-        });
+        onBackPressed();
     }
 
 }
